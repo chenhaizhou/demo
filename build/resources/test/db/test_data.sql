81747308a1b0fa15fa0ecc9efb8a3609c39ab4b0
chenhaizhou <hzchen@CNhzchen.local>
2014-10-15 14:30:12 +0800

    update

---
+++ build/resources/test/db/test_data.sql
@@ -0,0 +1,131 @@
+use lib;
+
+insert into users(user_name, password, name) values('admin', 'admin', 'Admin');
+
+insert into images (image_url) values('upload/31piGMTOzLL._SS120_RO10,1,201,225,243,255,255,255,15_.jpg');
+insert into images (image_url) values('upload/41-pwPn1koL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/41GxgvEPgqL._US130_.jpg');
+insert into images (image_url) values('upload/41JBTFpDVOL._SL121_PE30_OU28_.jpg');
+insert into images (image_url) values('upload/41nWjx+zsGL._US88_.jpg');
+insert into images (image_url) values('upload/41U72cnzFgL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/41XHXHhUOfL._SS120_RO10,1,201,225,243,255,255,255,15_ (1).jpg');
+insert into images (image_url) values('upload/41XHXHhUOfL._SS120_RO10,1,201,225,243,255,255,255,15_.jpg');
+insert into images (image_url) values('upload/41Y3d7LHs8L._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/41ywlt1rlCL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51AX1t2xQcL._SS120_RO10,1,201,225,243,255,255,255,15_.jpg');
+insert into images (image_url) values('upload/51b3ej4O2OL._SL121_PE31_OU28_.jpg');
+insert into images (image_url) values('upload/51bDmHvmPrL._SL110_.jpg');
+insert into images (image_url) values('upload/51C1vmS80nL._SL121_PE33_OU28_.jpg');
+insert into images (image_url) values('upload/51dkPLvhiAL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51e76NtvttL._SL121_PE32_OU28_.jpg');
+insert into images (image_url) values('upload/51fAM6h4EBL._SL121_PE26_OU28_.jpg');
+insert into images (image_url) values('upload/51g5-wSOneL._SL121_PE25_OU28_.jpg');
+insert into images (image_url) values('upload/51g7HOanzPL._SS120_RO10,1,201,225,243,255,255,255,15_.jpg');
+insert into images (image_url) values('upload/51kbATuMTLL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51kKwHAb3ZL._SL110_.jpg');
+insert into images (image_url) values('upload/51lSUgviLTL._SL121_PE31_OU28_.jpg');
+insert into images (image_url) values('upload/51QE4mFcKCL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51qoYr1IqEL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51qVegN225L._SS120_RO10,1,201,225,243,255,255,255,15_.jpg');
+insert into images (image_url) values('upload/51txKr-RYKL._SL110_PIsitb-sticker-arrow-sm,TopRight,10,-13_OU28_.jpg');
+insert into images (image_url) values('upload/51wJvv4zZSL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51ZbJqVrm5L._SL121_PE28_OU28_.jpg');
+insert into images (image_url) values('upload/61nNz1aB0dL._SL110_.jpg');
+insert into images (image_url) values('upload/61u02+ZT9zL._SL110_.jpg');
+insert into images (image_url) values('upload/411aRXH0vRL._SL110_.jpg');
+insert into images (image_url) values('upload/413m5i3LA+L._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/512DpqSPUIL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/512evUqoqwL._SL150_.jpg');
+insert into images (image_url) values('upload/515K6ZtAl2L._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL121_PE36_OU28_.jpg');
+insert into images (image_url) values('upload/516dHSz2JaL._SS120_RO10,1,201,225,243,255,255,255,15_.jpg');
+insert into images (image_url) values('upload/516dHSz2JaL._US88_.jpg');
+insert into images (image_url) values('upload/518fJU-ijYL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/613U+xs0X3L._SL121_PE39_OU28_.jpg');
+insert into images (image_url) values('upload/613U+xs0X3L._US88_.jpg');
+insert into images (image_url) values('upload/51698VQDSpL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+insert into images (image_url) values('upload/51698VQDSpL._SL500_PIsitb-sticker-arrow-big,TopRight,35,-73_SL150_OU28_.jpg');
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Deals worth more than', 'Cathy Wu', '2308089-20349380_1', 'dsfas', 1, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('$18 billion ', 'Cathy Wu', '2308089-20349380_2', 'dsfas', 2, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('were signed as Premier ', 'Cathy Wu', '2308089-20349380_3', 'dsfas', 3, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Li Keqiang met German Chancellor ', 'Cathy Wu', '2308089-20349380_4', 'dsfas', 4, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Angela Merkel on Friday ', 'Cathy Wu', '2308089-20349380_5', 'dsfas', 5, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('in the third round of top-level talks ', 'Cathy Wu', '2308089-20349380_6', 'dsfas', 6, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('this year between the Asian ', 'Cathy Wu', '2308089-20349380_7', 'dsfas', 7, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('European export powerhouses', 'Cathy Wu', '2308089-20349380_8', 'dsfas', 8, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Airbus SAS and China Aviation Supplies ', 'Cathy Wu', '2308089-20349380_9', 'dsfas', 9, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Holding Co signed a general terms agreement for China ', 'Cathy Wu', '2308089-20349380_10', 'dsfas', 10, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('to buy 70 A320 Airbuses', 'Cathy Wu', '2308089-20349380_11', 'dsfas', 11, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('The two sides also signed ', 'Cathy Wu', '2308089-20349380_12', 'dsfas', 12, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('a letter of intent reconfirming', 'Cathy Wu', '2308089-20349380_13', 'dsfas', 13, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('their intention to set up', 'Cathy Wu', '2308089-20349380_14', 'dsfas', 14, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('an A330 completion and delivery center in Tianjin', 'Cathy Wu', '2308089-20349380_15', 'dsfas', 15, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Other deals included agriculture', 'Cathy Wu', '2308089-20349380_16', 'dsfas', 16, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('automobiles, telecoms, healthcare and education', 'Cathy Wu', '2308089-20349380_17', 'dsfas', 17, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('China and Germany also signed ', 'Cathy Wu', '2308089-20349380_18', 'dsfas', 18, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('a wide-ranging guideline covering 110 cooperation agreements', 'Cathy Wu', '2308089-20349380_19', 'dsfas', 19, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('over the next five to 10 years', 'Cathy Wu', '2308089-20349380_20', 'dsfas', 20, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('the largest agreement of its kind that China has reached with other countries', 'Cathy Wu', '2308089-20349380_21', 'dsfas', 21, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Li was welcomed by Merkel and German President Joachim Gauck', 'Cathy Wu', '2308089-20349380_22', 'dsfas', 22, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('at a ceremony in Berlin with full military honors', 'Cathy Wu', '2308089-20349380_23', 'dsfas', 23, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('The premier was accompanied by government officials', 'Cathy Wu', '2308089-20349380_24', 'dsfas', 24, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('an entourage of 130 business chiefs', 'Cathy Wu', '2308089-20349380_25', 'dsfas', 25, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Both sides then met in a joint government meeting', 'Cathy Wu', '2308089-20349380_26', 'dsfas', 26, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Both sides stressed that they want to', 'Cathy Wu', '2308089-20349380_27', 'dsfas', 27, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('boost their multi-billion-dollar trade and investment ties', 'Cathy Wu', '2308089-20349380_28', 'dsfas', 28, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('deepen a new innovation partnership', 'Cathy Wu', '2308089-20349380_29', 'dsfas', 29, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('The two sides have deeply engaged', 'Cathy Wu', '2308089-20349380_30', 'dsfas', 30, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('like gearwheels meshing with each other', 'Cathy Wu', '2308089-20349380_31', 'dsfas', 31, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('to form an interwoven interest group', 'Cathy Wu', '2308089-20349380_32', 'dsfas', 32, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Li said during an address', 'Cathy Wu', '2308089-20349380_33', 'dsfas', 33, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('to the Sino-German Economic', 'Cathy Wu', '2308089-20349380_34', 'dsfas', 34, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Technical Forum', 'Cathy Wu', '2308089-20349380_35', 'dsfas', 35, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Experts said this indicates', 'Cathy Wu', '2308089-20349380_36', 'dsfas', 36, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('the two driving forces in Europe', 'Cathy Wu', '2308089-20349380_37', 'dsfas', 37, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('Asia are focusing ', 'Cathy Wu', '2308089-20349380_38', 'dsfas', 38, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('less on low-end ', 'Cathy Wu', '2308089-20349380_39', 'dsfas', 39, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('manufacturing and more on high-tech fields', 'Cathy Wu', '2308089-20349380_40', 'dsfas', 40, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('The premiers visit to Germany comes as Merkel', 'Cathy Wu', '2308089-20349380_41', 'dsfas', 41, 'asdfasdfasdfasdf',now());
+
+insert into books (book_name, author, isbn, publisher, cover_image_id, introduction, create_time) values ('looks to increase exports outside Europe', 'Cathy Wu', '2308089-20349380_42', 'dsfas', 42, 'asdfasdfasdfasdf',now());
+
